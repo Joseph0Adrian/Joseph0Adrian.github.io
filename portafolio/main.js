4127d6c18bc30be3ac37d8cf08290a5f1698bdfb
Joseph <cjoseph.adrian@gmail.com>
2021-04-21 19:31:08 -0500

    cambiando diseño

---
+++ portafolio/main.js
@@ -0,0 +1,20 @@
+const orangeStarUrl = document.getElementById('orange-star-url');
+const smokejumpersUrl = document.getElementById('smokejumpers-url');
+const parallaxUrl = document.getElementById('parallax-url');
+const clonNetflixUrl = document.getElementById('clon-netflix-url');
+
+orangeStarUrl.addEventListener('click', () => {
+    window.location.href='https://joseph0adrian.github.io/orange-star-index';
+});
+
+smokejumpersUrl.addEventListener('click', () => {
+    window.location.href='https://joseph0adrian.github.io/figma-smokejumper/';
+});
+
+parallaxUrl.addEventListener('click', () => {
+    window.location.href='https://joseph0adrian.github.io/efecto-parallax/';
+});
+
+clonNetflixUrl.addEventListener('click', () => {
+    window.location.href='https://joseph0adrian.github.io/neflix/';
+});
