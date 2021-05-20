c96c6d63cc4ddf17739ce942fc17e03021dc6ff8
Joseph <cjoseph.adrian@gmail.com>
2021-05-20 18:19:48 -0500

    ejemplo terminado

--- python/blog2/app.py
+++ python/blog2/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\Joseph\\Documents\\sublime-text\\Joseph0Adrian.github.io\\python\blog\\blog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\Joseph\\Documents\\sublime-text\\Joseph0Adrian.github.io\\python\\blog2\\blog2.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
