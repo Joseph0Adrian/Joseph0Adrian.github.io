5a4837888f833e3c612aa3bd5cd802224ee101a3
Joseph <cjoseph.adrian@gmail.com>
2021-05-22 20:20:45 -0500

    agregando vue-app a portafolio

--- portafolio/main.js
+++ portafolio/main.js
@@ -2,6 +2,7 @@ const orangeStarUrl = document.getElementById('orange-star-url');
 const smokejumpersUrl = document.getElementById('smokejumpers-url');
 const parallaxUrl = document.getElementById('parallax-url');
 const clonNetflixUrl = document.getElementById('clon-netflix-url');
+const vueApp = document.getElementById('vue-app-url');
 
 orangeStarUrl.addEventListener('click', () => {
     window.location.href='https://joseph0adrian.github.io/orange-star-index';
@@ -17,4 +18,8 @@ parallaxUrl.addEventListener('click', () => {
 
 clonNetflixUrl.addEventListener('click', () => {
     window.location.href='https://joseph0adrian.github.io/neflix/';
+});
+
+vueApp.addEventListener('click', () => {
+    window.location.href='https://joseph0adrian.github.io/framework-vue/tutorial-vue2/dist/';
 });
