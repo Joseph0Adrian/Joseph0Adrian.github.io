1f390fae9a5a94215d05010155df3385e9511465
Joseph <cjoseph.adrian@gmail.com>
2021-08-03 18:39:43 -0500

    actualizando

--- framework-vue/pokedex2/src/components/PokedexFormulario.vue
+++ framework-vue/pokedex2/src/components/PokedexFormulario.vue
@@ -14,6 +14,7 @@
             v-on:click="search"
         />
     </form>
+    <br>
 </template>
 
 <script>
--- framework-vue/pokedex2/src/components/PokedexPantalla.vue
+++ framework-vue/pokedex2/src/components/PokedexPantalla.vue
@@ -1,58 +1,73 @@
 <template>
-    <div class="pokedex-pantalla">
-        <div class="pokemon-info">
-            <img
-                class="pokemon-img"
+    <div class="pokedex-pantalla container">
+        <div class="pokemon-info row">
+            <div class="card col" style="width: 18rem;">
+                <img
+                class="pokemon-img card-img-top"
                 v-bind:src="imageurl + pokemon.id + '.png'"
                 v-bind:alt="pokemon.name" />
-            <h2 class="pokemon-name">{{pokemon.name}}</h2>
-            <ul>
-                <li>height: {{pokemon.height}}</li>
-                <li>weight: {{pokemon.weight}}</li>
-                <Stat
-                    v-bind:item= "item"
-                    v-for= "item in pokemon.stats"
-                    v-bind:key= "item.stat.name"
-                />
-                <br>
-                type
-                <Type
-                    v-bind:item= "item"
-                    v-for= "item in pokemon.types"
-                    v-bind:key= "item.slot"
-                />
-                <br>
-                abilities
-                <Abilities
-                    v-bind:item= "item"
-                    v-for= "item in pokemon.abilities"
-                    v-bind:key= "item.ability"
-                />
-            </ul>
+                <div class="card-body">
+                    <h5 class=" pokemon-name card-title">{{pokemon.name}}</h5>
+                    <ul class="card-text">
+                        Stat
+                        <li>height: {{pokemon.height}}</li>
+                        <li>weight: {{pokemon.weight}}</li>
+                        <Stat
+                            v-bind:item= "item"
+                            v-for= "item in pokemon.stats"
+                            v-bind:key= "item.stat.name"
+                        />
+                        <br>
+                        Type
+                        <Type
+                            v-bind:item= "item"
+                            v-for= "item in pokemon.types"
+                            v-bind:key= "item.slot"
+                        />
+                        <br>
+                        Abilities
+                        <Abilities
+                            v-bind:item= "item"
+                            v-for= "item in pokemon.abilities"
+                            v-bind:key= "item.ability"
+                        />
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import Stat from './Stat.vue'
-import Type from './Type.vue'
-import Abilities from './Abilities'
-export default {
-    name: 'PokedexPantalla',
-    components: {
-        Stat, Type, Abilities
-    },
-    data(){
-        return{
-            imageurl:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
-        }
-    },
-    props: {
-    // PropTypes on Vue
-        pokemon: {
-            type: Object,
-            default: () => {}
+    import Stat from './Stat.vue'
+    import Type from './Type.vue'
+    import Abilities from './Abilities'
+    export default {
+        name: 'PokedexPantalla',
+        components: {
+            Stat, Type, Abilities
         },
-    },
-}
+        data(){
+            return{
+                imageurl:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+            }
+        },
+        props: {
+        // PropTypes on Vue
+            pokemon: {
+                type: Object,
+                default: () => {}
+            },
+        },
+    }
 </script>
+
+<style scoped>
+    .card-body{
+        text-align: left
+    }
+
+    .pokemon-name{
+        text-align: center
+    }
+</style>
