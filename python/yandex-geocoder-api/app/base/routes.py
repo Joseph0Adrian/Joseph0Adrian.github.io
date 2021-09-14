3d9e196b631586743db8a6b34b860bf360d17cd2
Joseph <cjoseph.adrian@gmail.com>
2021-09-14 17:54:03 -0500

    actualizando

--- python/yandex-geocoder-api/app/base/routes.py
+++ python/yandex-geocoder-api/app/base/routes.py
@@ -7,7 +7,7 @@ from . import base
 
 @base.route('/search-by-address')
 def search_by_address():
-    addres = 'territoriya Sportivny klub Romashkovo'    
+    addres = 'territoriya Sportivny klub Romashkovo'
     try:
         response = requests.request("GET", "https://geocode-maps.yandex.ru/1.x/?apikey=823764c6-f937-43cb-a80d-1d8118746554&format=json&geocode=" +addres+ "&lang=en-US")
         if response.status_code != 200:
