42c4a390eff9bd54365ccd3147ee4999a798fb04
Joseph <cjoseph.adrian@gmail.com>
2021-09-27 18:11:11 -0500

    actualizando

--- python/yandex-geocoder-api-2/api/requests_api.py
+++ python/yandex-geocoder-api-2/api/requests_api.py
@@ -3,8 +3,7 @@ import requests
 class RequestsApi:
 
     @staticmethod
-    def get_coordinate_by_address():
-        addres = 'territoriya Sportivny klub Romashkovo'
+    def get_coordinate_by_address(addres):
         try:
             response = requests.request("GET", "https://geocode-maps.yandex.ru/1.x/?apikey=823764c6-f937-43cb-a80d-1d8118746554&format=json&geocode=" +addres+ "&lang=en-US")
             if response.status_code != 200:
--- python/yandex-geocoder-api-2/app.py
+++ python/yandex-geocoder-api-2/app.py
@@ -11,11 +11,21 @@ from api.requests_api import RequestsApi
 app=Flask(__name__)
 app.secret_key = "823764c6-f937-43cb-a80d-1d8118746554"
 
-@app.route('/view')
-def view():
-    res = RequestsApi.get_coordinate_by_address()
-    point = res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-    return render_template('view.html', point=point)
+@app.route('/new')
+def new():
+    return render_template('search.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == 'POST':
+        try:
+            value_input = request.form['value_input']
+            res = RequestsApi.get_coordinate_by_address(value_input)
+            point = res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+            name = res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['name']
+            return render_template('view.html', name=name, point=point)
+        except:
+            return "not saved"                
 
 if __name__ == '__main__':
     app.run(debug=True)
