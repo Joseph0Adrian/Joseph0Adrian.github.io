a40ce39c6a1050d31fc7a631bf4e3b55c5aa9a85
Joseph <cjoseph.adrian@gmail.com>
2022-04-17 19:07:24 -0500

    actualizando

--- rutas-urbanas/prototipo-celular/javascript/main.js
+++ rutas-urbanas/prototipo-celular/javascript/main.js
@@ -1,12 +1,9 @@
 // inicializa Leaflet
-var map = L.map('map').setView([18.893920, -96.929300], 16);
+var map = L.map('map').setView([18.893920, -96.929300], 13);
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // muestra la barra de escala en la esquina inferior izquierda
