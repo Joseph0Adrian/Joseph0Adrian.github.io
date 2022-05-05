b5350103ef12f518696858259252fff2694fe583
Joseph <cjoseph.adrian@gmail.com>
2022-05-04 19:10:13 -0500

    al cerrar menu paradas ruta se borra la ruta del mapa

--- rutas-urbanas/prototipo-celular/javascript/main.js
+++ rutas-urbanas/prototipo-celular/javascript/main.js
@@ -4,6 +4,21 @@ var rutaBuenaVista;
 var rutaLaEstacion;
 var grupoCapas = new L.layerGroup();
 
+// inicializa Leaflet
+var map = L.map('map', {
+    center: [18.893920, -96.929300],
+    zoom: 14,
+    zoomControl: false
+});
+
+// add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// muestra la barra de escala en la esquina inferior izquierda
+L.control.scale({ imperial: true, metric: true }).addTo(map);
+
 /* Abre cuando hacen click en el boton */
 function abrirMenuRutas() {
     document.getElementById("menuRutas").style.height = "100%";
@@ -20,23 +35,16 @@ function abrirMenuParadasRutas() {
 
 function cerrarMenuParadasRutas() {
     document.getElementById("menuParadasRutas").style.width = "0%";
+    quitarParadasAutobus();
+    map.removeLayer(rutaBuenaVista);
     abrirMenuRutas();
 }
 
-// inicializa Leaflet
-var map = L.map('map', {
-    center: [18.893920, -96.929300],
-    zoom: 14,
-    zoomControl: false
-});
-
-// add the OpenStreetMap tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// muestra la barra de escala en la esquina inferior izquierda
-L.control.scale({ imperial: true, metric: true }).addTo(map);
+function quitarParadasAutobus() {
+    for (let i = 0; i < marcadores.length; i++) {
+        map.removeLayer(marcadores[i]);
+    }
+}
 
 function agregandoParadasAutobus(arrayConParadas) {
     for (let i = 0; i < arrayConParadas.length; i++) {
