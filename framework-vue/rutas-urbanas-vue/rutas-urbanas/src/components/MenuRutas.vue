b9399a9c432b5b5a10bc2c8ccb1baef1685bd34f
Joseph <cjoseph.adrian@gmail.com>
2022-11-16 22:22:53 -0600

    pruebas leaflet con vue 2

--- framework-vue/rutas-urbanas-vue/rutas-urbanas/src/components/MenuRutas.vue
+++ framework-vue/rutas-urbanas-vue/rutas-urbanas/src/components/MenuRutas.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="col-sm-2">
+  <div class="col-sm-2 vh-100">
       <h3>Menu rutas</h3>
-      <button class="btn btn-danger">Rutas</button>
+      <button class="btn btn-danger" v-on:click='getCoordenadas()'>Rutas</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MenuRutas',
-  props: {
-  }
+  data(){
+    return{
+      // coordendas:null,
+      msg: null,
+      coordenadas:[0,0],
+    };
+  },
+  created(){
+    // this.getCoordenadas()
+  },
+  methods:{
+    getCoordenadas(){
+      fetch(`http://localhost/rutasUrbanas/api/read.php`).then(res => res.json()).then(data => {
+        // this.coordenadas = data;
+        this.arrayCoordenadas(data);
+        this.msg = this.coordenadas;
+        // this.msg = data;
+        // console.log(data.body[0].coordenadaX);
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    arrayCoordenadas(data){
+      this.coordenadas.pop();
+      this.coordenadas.pop();
+      for (let index = 0; index < data.body.length; index++) {
+        this.coordenadas.push(data.body[index].coordenadaX, data.body[index].coordenadaY);
+      }
+      // console.log(this.coordenadas);
+    },
+  },
+  watch: {
+    msg: function() {
+      this.$root.$emit('send', this.msg);
+    },
+  },
 }
 </script>
 
--- framework-vue/rutas-urbanas-vue/rutas-urbanas/src/components/RutasUrbanas.vue
+++ framework-vue/rutas-urbanas-vue/rutas-urbanas/src/components/RutasUrbanas.vue
@@ -1,31 +1,66 @@
 <template>
-  <div class="col-sm-10">
+  <div class="col-sm-10 vh-100">
       <l-map :zoom="zoom" :center="center">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        <l-marker :lat-lng="markerLatLng"></l-marker>
+        <l-polyline :lat-lngs="polyline.latlngs" :color="polyline.color"></l-polyline>
+        <!-- <l-marker :lat-lng="markerLatLng"></l-marker> -->
       </l-map>
+      <p>
+        <strong>
+          Mensaje del extraño:
+        </strong>
+      </p>
+      <p>{{ msgOfOther }}</p>
   </div>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import {LMap, LTileLayer, LPolyline} from 'vue2-leaflet';
 export default {
   name: 'RutasUrbanas',
+  props:['msg'],
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LPolyline,
+    // LMarker,
   },
   data () {
     return {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
-    };
-  }
+      zoom: 13,
+      center: [18.893920, -96.929300],
+      polyline: {
+        latlngs: [[18.893920, -96.929300]],
+		color: 'green'
+      },
+      // markerLatLng: [51.504, -0.159],
+      msgOfOther: [],
+    }
+  },
+  created(){
+
+  },
+  beforeMount() {
+    this.$root.$on('send', data => {
+    console.log(data);
+      if(data!=null){
+        this.polyline.latlngs = data;
+      } else {
+        this.polyline.latlngs = [[18.893920, -96.929300]];
+      }
+      // this.msgOfOther = data;
+      // console.log(this.msgOfOther);
+    });
+  },
+  methods:{
+    mostrarRuta(){
+      // console.log();
+      // this.latlngs=this.msgOfOther;
+    },
+  },
 }
 </script>
 
